064f3fb5fab955bcb8363f58580c006acdc82d05
NikithaChandrashekar <72256727+NikithaChandrashekar@users.noreply.github.com>
2021-01-02 21:08:38 +0530

    Update Chain.js

--- Chain.js
+++ Chain.js
@@ -6,8 +6,8 @@ class Chain{
         var options = {
             bodyA: body1,
             bodyB:body2,
-           // stiffness: 0.04,
-           // length: 10,
+           
+           
             pointB:{x:this.offsetX,y:this.offsetY}
     
         }
